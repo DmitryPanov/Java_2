a350d7ec8fe89ae7ed83a0d334d1595201db1499
Dmitry <d.panov27@gmail.com>
2020-04-08 20:58:22 +0300

    All task

---
+++ src/lesson2/LengthArrayException.java
@@ -0,0 +1,8 @@
+package lesson2;
+
+public class LengthArrayException extends Exception {
+
+    public LengthArrayException (){
+        System.out.println("Длина Массива не равна 4");
+    }
+}
--- src/lesson2/Main.java
+++ src/lesson2/Main.java
@@ -5,16 +5,21 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] arg) {
         String s = "10 3 1 2\n2 3 2 2\n5 6 7 1\n300 3 1 0";
-        System.out.println(Arrays.deepToString(changeString(s)));
-        System.out.println("Sum " + sumArray(changeString(s)));
+
+        try {
+            System.out.println("Sum " + sumArray(changeString(s)));
+        } catch (NotANumberException | LengthArrayException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    public static String[][] changeString(String s) {
+    public static String[][] changeString(String s) throws LengthArrayException {
         int count = 0;
         int length = 4;
         String[] str = s.replace("\n", " ").split(" ");
         String[][] arr = new String[length][length];
+        if (length != 4) throw new LengthArrayException();
         for (int i = 0; i < length; i++) {
             for (int j = 0; j < length; j++) {
                 arr[i][j] = str[count];
@@ -24,11 +29,16 @@ public class Main {
         return arr;
     }
 
-    public static float sumArray(String[][] arr) {
+    public static float sumArray(String[][] arr) throws NotANumberException {
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
-                sum += Integer.parseInt(arr[i][j]);
+                try{
+                    sum += Integer.parseInt(arr[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new NotANumberException();
+                }
+
             }
         }
         return sum * 1.0f / 2;
