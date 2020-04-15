368d3cee6b33bcdabc0e3e52de31cc673b3ea860
Dmitry <d.panov27@gmail.com>
2020-04-15 23:38:37 +0300

    First task + Second task . In the second task I just described the method

---
+++ src/lesson4/ClientGUI.java
@@ -0,0 +1,109 @@
+package lesson4;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ClientGUI extends JFrame implements ActionListener, Thread.UncaughtExceptionHandler {
+
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 300;
+
+    private final JTextArea log = new JTextArea();
+    private final JPanel panelTop = new JPanel(new GridLayout(2, 3));
+    private final JTextField tfIPAddress = new JTextField("127.0.0.1");
+    private final JTextField tfPort = new JTextField("8189");
+    private final JCheckBox cbAlwaysOnTop = new JCheckBox("Always on top", true);
+    private final JTextField tfLogin = new JTextField("ivan");
+    private final JPasswordField tfPassword = new JPasswordField("123");
+    private final JButton btnLogin = new JButton("Login");
+
+    private final JPanel panelBottom = new JPanel(new BorderLayout());
+    private final JButton btnDisconnect = new JButton("<html><b>Disconnect</b></html>");
+    private final JTextField tfMessage = new JTextField();
+    private final JButton btnSend = new JButton("Send");
+
+    private final JList<String> userList = new JList<>();
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ClientGUI();
+            }
+        });
+    }
+
+    ClientGUI() {
+        Thread.setDefaultUncaughtExceptionHandler(this);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(WIDTH, HEIGHT);
+        setAlwaysOnTop(true);
+        userList.setListData(new String[]{"user1", "user2", "user3", "user4",
+                "user5", "user6", "user7", "user8", "user9",
+                "user-with-exceptionally-long-name-in-this-chat"});
+        JScrollPane scrUser = new JScrollPane(userList);
+        JScrollPane scrLog = new JScrollPane(log);
+        scrUser.setPreferredSize(new Dimension(100, 0));
+        log.setLineWrap(true);
+        log.setWrapStyleWord(true);
+        log.setEditable(false);
+        cbAlwaysOnTop.addActionListener(this);
+        tfMessage.addActionListener(this);
+        btnSend.addActionListener(this);
+
+        panelTop.add(tfIPAddress);
+        panelTop.add(tfPort);
+        panelTop.add(cbAlwaysOnTop);
+        panelTop.add(tfLogin);
+        panelTop.add(tfPassword);
+        panelTop.add(btnLogin);
+        panelBottom.add(btnDisconnect, BorderLayout.WEST);
+        panelBottom.add(tfMessage, BorderLayout.CENTER);
+        panelBottom.add(btnSend, BorderLayout.EAST);
+
+        add(scrUser, BorderLayout.EAST);
+        add(scrLog, BorderLayout.CENTER);
+        add(panelTop, BorderLayout.NORTH);
+        add(panelBottom, BorderLayout.SOUTH);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object src = e.getSource();
+        if (src == cbAlwaysOnTop) {
+            setAlwaysOnTop(cbAlwaysOnTop.isSelected());
+        } else if (src == tfMessage || src == btnSend ) {
+            log.append(tfMessage.getText() + "\n");
+            /*Если правильно понял 2-е задание, то нужно просто указать
+            место где планировал записывать данный с лога в файл.
+            Выбрал бы данное место.По действиям:
+            1.Указываем путь к расположения файла "C:\\Users\\....."
+            2.Создаем экземпляр класс FileWriter
+            3.Записываем. fWriter.write(tfMessage.getText() + "\n");
+            4.Закрываем.
+             */
+            tfMessage.setText("");
+        } else {
+            throw new RuntimeException("Unknown source:" + src);
+        }
+
+    }
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        e.printStackTrace();
+        String msg;
+        StackTraceElement[] ste = e.getStackTrace();
+        msg = String.format("Exception in \"%s\" %s: %s\n\tat %s",
+                t.getName(), e.getClass().getCanonicalName(), e.getMessage(), ste[0]);
+        JOptionPane.showMessageDialog(this, msg, "Exception", JOptionPane.ERROR_MESSAGE);
+        System.exit(1);
+    }
+}
